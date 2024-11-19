aaeb24b7061985b145f88f1d02b9e1daf196b8da
DDR23 <roboreserva.andre@gmail.com>
2024-11-18 21:20:16 -0300

    feat: adicionando background animado na pagina inicial

--- src/app/(home)/page.tsx
+++ src/app/(home)/page.tsx
@@ -1,40 +1,48 @@
-'use client'
+'use client';
 import { Carousel } from '@mantine/carousel';
-import { Stack } from '@mantine/core';
+import { BackgroundImage, Card, Center } from '@mantine/core';
+import { useRef } from 'react';
+import Autoplay from 'embla-carousel-autoplay';
 
 const backgrounds = [
-  {
-    src: 'primeira',
-    color: 'blue'
-  },
-  {
-    src: 'segunda',
-    color: 'red'
-  },
-  {
-    src: 'terceira',
-    color: 'green'
-  },
-]
+  { src: './picture_01.png' },
+  { src: './picture_02.png' },
+  { src: './picture_03.png' },
+];
 
 export default function Home() {
+  const autoplay = useRef(Autoplay({ delay: 4000 }));
 
   const slides = backgrounds.map((row, index) => (
     <Carousel.Slide key={index}>
-      <Stack justify='center' ta='center' h='100vh' bg={row.color} pb='100'>
-        {row.src}
-      </Stack>
+      <BackgroundImage
+        src={row.src}
+        h='100vh'
+        opacity={0.7}
+      >
+      </BackgroundImage>
     </Carousel.Slide>
-  ))
+  ));
 
   return (
-    // TODO - meu carousel deve ficar dentro do backgroundImage
-    // TODO - meu card de login d register deve ser estatico
-    <Carousel
-      bg='dark'
-      loop
-    >
-      {slides}
-    </Carousel>
+    <>
+      <Carousel
+        loop
+        withControls={false}
+        draggable={false}
+        plugins={[autoplay.current]}
+      >
+        {slides}
+      </Carousel>
+      <Center h='100vh' w='100vw' pb='60px' style={{
+        position: 'absolute',
+        zIndex: 10,
+        top: 0,
+      }}>
+        <Card w='20rem' h='20rem' bg='background'>
+          teste
+        </Card>
+      </Center>
+    </>
   );
 }
--- src/components/header/header.tsx
+++ src/components/header/header.tsx
@@ -3,7 +3,7 @@ import { Group, Image } from '@mantine/core';
 export default function Header() {
   return (
     <Group pos='fixed' top='0' w='100%' justify='center' bg='background' style={{ borderBottom: '1px solid #23232320', zIndex: '10' }}>
-      <Image src='/logo.png' h='60' />
+      <Image src='/logo.png' h='60px' />
     </Group>
   );
 }
