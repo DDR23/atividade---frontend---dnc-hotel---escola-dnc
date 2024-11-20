124e71953c52cdd56e6438d4bd43ef59c50333f6
DDR23 <roboreserva.andre@gmail.com>
2024-11-20 01:09:39 -0300

    fix: area de cadastro agora está funcional

--- src/components/auth/signUp.tsx
+++ src/components/auth/signUp.tsx
@@ -1,59 +1,51 @@
-import { schemaSignup } from "@/schemas/auth/schemaSingUp";
-import PasswordStrength, { requirements } from "@/utils/passwordStrength";
+import usePost from "@/hooks/usePost";
+import { schemaAuth } from "@/schemas/auth/schemaAuth";
+import PasswordStrength, { getStrength } from "@/utils/passwordStrength";
 import { yupResolver } from "@hookform/resolvers/yup";
 import { Button, Group, PasswordInput, TextInput } from "@mantine/core";
-import { notifications } from "@mantine/notifications";
-import { IconX } from "@tabler/icons-react";
-import { useState } from "react";
-import { SubmitHandler, useForm } from "react-hook-form";
+import { useEffect } from "react";
+import { useForm } from "react-hook-form";
+import NotificationShow from "../_ui/notification/notificationShow";
 
-interface SignupPostValues {
-  USER_NAME?: string
-  USER_PASSWORD?: string
+interface UsePostReq {
+  USER_EMAIL: string;
+  USER_PASSWORD: string;
 }
 
-function validatePassword(password: string): string[] {
-  const errors = [];
-  if (password.length <= 5) {
-    errors.push("A senha deve ter pelo menos 6 caracteres");
-  }
-  requirements.forEach(requirement => {
-    if (!requirement.re.test(password)) {
-      errors.push(requirement.label);
-    }
-  });
-  return errors;
+interface UsePostRes {
+  access_token: string;
 }
 
 export default function SignUp() {
   const { register, handleSubmit, formState: { errors }, watch } = useForm({
     mode: 'onChange',
-    resolver: yupResolver(schemaSignup)
+    resolver: yupResolver(schemaAuth),
   });
-  const watchPassword = watch("USER_PASSWORD", "");
-  const [posted, setPosted] = useState(false);
-  const [data, setData] = useState<SignupPostValues>({ USER_NAME: '', USER_PASSWORD: '' });
 
-  const submitForm: SubmitHandler<SignupPostValues> = (formData) => {
-    const passwordErrors = validatePassword(formData.USER_PASSWORD || '');
-    if (passwordErrors.length > 0) {
-      notifications.show({
-        title: 'Senha Invalida',
-        message: `Precisa ${passwordErrors.join(', ')}`,
-        autoClose: 8000,
-        color: 'red',
-        icon: <IconX />
+  const watchData = watch();
+  const passwordStrength = getStrength(watchData.USER_PASSWORD || "");
+  const isPasswordValid = passwordStrength === 100;
+
+  const { isPosting, response, error, sendRequest } = usePost<UsePostReq, UsePostRes>(`${process.env.NEXT_PUBLIC_BASE_URL}/auth/login`, watchData);
+
+  useEffect(() => {
+    if (error) {
+      NotificationShow({
+        title: 'Erro',
+        message: 'Ocorreu um erro durante o cadastro.',
       });
-      return;
     }
-    setData(formData);
-    setPosted(true);
-  };
-  console.log('data', data)
-  console.log('posted', posted)
+
+    if (response) {
+      NotificationShow({
+        title: 'Sucesso',
+        message: 'Usuário cadastrado com sucesso!',
+      });
+    }
+  }, [error, response]);
 
   return (
-    <form onSubmit={handleSubmit(submitForm)}>
+    <form onSubmit={handleSubmit(sendRequest)}>
       <TextInput
         {...register('USER_EMAIL')}
         label="Email"
@@ -63,9 +55,16 @@ export default function SignUp() {
         {...register('USER_PASSWORD')}
         label="Senha"
       />
-      <PasswordStrength value={watchPassword} />
+      <PasswordStrength value={watchData.USER_PASSWORD || ''} />
       <Group justify="flex-end" mt="md">
-        <Button fullWidth type="submit">Cadastrar</Button>
+        <Button
+          fullWidth
+          type="submit"
+          disabled={isPosting || !isPasswordValid}
+          loading={isPosting}
+        >
+          Cadastrar
+        </Button>
       </Group>
     </form>
   );
--- src/utils/passwordStrength.tsx
+++ src/utils/passwordStrength.tsx
@@ -2,57 +2,42 @@ import { Box, Progress, Text, Center, Group } from '@mantine/core';
 import { IconCheck, IconX } from '@tabler/icons-react';
 
 interface Props {
-  value: string
+  value: string;
 }
 
-function PasswordRequirement({ meets, label }: { meets: boolean; label: string }) {
-  return (
-    <Text component="div" c={meets ? 'teal' : 'red'} mt={5} size="sm">
-      <Center inline>
-        {meets ? <IconCheck size="0.9rem" stroke={1.5} /> : <IconX size="0.9rem" stroke={1.5} />}
-        <Box ml={7}>{label}</Box>
-      </Center>
-    </Text>
-  );
-}
-
-export const requirements = [
-  { re: /[0-9]/, label: 'conter número' },
-  { re: /[a-z]/, label: 'conter letra minúscula' },
-  { re: /[A-Z]/, label: 'conter letra maiúscula' },
-  { re: /[$&+,:;=?@#|'<>.^*()%!-]/, label: 'conter caractere especial' },
+const requirements = [
+  { re: /[0-9]/, label: 'Conter número' },
+  { re: /[a-z]/, label: 'Conter letra minúscula' },
+  { re: /[A-Z]/, label: 'Conter letra maiúscula' },
+  { re: /[$&+,:;=?@#|'<>.^*()%!-]/, label: 'Conter caractere especial' },
 ];
 
-function getStrength(password: string) {
-  let multiplier = password.length > 5 ? 0 : 1;
+export const getStrength = (password: string) => {
+  const unmet = requirements.filter(req => !req.re.test(password)).length;
+  const baseScore = password.length > 5 ? 0 : 1; // Penalidade para senhas curtas
+  const penalty = unmet + baseScore;
+  return Math.max(100 - (100 / (requirements.length + 1)) * penalty, 0);
+};
 
-  requirements.forEach((requirement) => {
-    if (!requirement.re.test(password)) {
-      multiplier += 1;
-    }
-  });
-
-  return Math.max(100 - (100 / (requirements.length + 1)) * multiplier, 0);
-}
+const PasswordRequirement = ({ meets, label }: { meets: boolean; label: string }) => (
+  <Text component='div' c={meets ? 'teal' : 'red.6'} size="sm" mt={5}>
+    <Center inline>
+      {meets ? <IconCheck size="0.9rem" stroke={1.5} /> : <IconX size="0.9rem" stroke={1.5} />}
+      <Box ml={7}>{label}</Box>
+    </Center>
+  </Text>
+);
 
 export default function PasswordStrength({ value }: Props) {
   const strength = getStrength(value);
-  const checks = requirements.map((requirement, index) => (
-    <PasswordRequirement key={index} label={requirement.label} meets={requirement.re.test(value)} />
+  const bars = Array.from({ length: 4 }, (_, index) => (
+    <Progress
+      key={index}
+      value={strength >= ((index + 1) / 4) * 100 ? 100 : 0}
+      color={strength > 80 ? 'teal' : strength > 50 ? 'yellow' : 'red'}
+      size={4}
+    />
   ));
-  const bars = Array(4)
-    .fill(0)
-    .map((_, index) => (
-      <Progress
-        styles={{ section: { transitionDuration: '0ms' } }}
-        value={
-          value.length > 0 && index === 0 ? 100 : strength >= ((index + 1) / 4) * 100 ? 100 : 0
-        }
-        color={strength > 80 ? 'teal' : strength > 50 ? 'yellow' : 'red'}
-        key={index}
-        size={4}
-      />
-    ));
 
   return (
     <div>
@@ -60,7 +45,9 @@ export default function PasswordStrength({ value }: Props) {
         {bars}
       </Group>
       <PasswordRequirement label="Conter pelo menos 6 caracteres" meets={value.length > 5} />
-      {checks}
+      {requirements.map((req, idx) => (
+        <PasswordRequirement key={idx} label={req.label} meets={req.re.test(value)} />
+      ))}
     </div>
   );
 }
